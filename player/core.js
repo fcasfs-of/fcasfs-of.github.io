9b1e0f46a5cdb58cc9c525553bfb813f45a0d69d
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-25 07:53:37 -0300

    Create core.js

---
+++ player/core.js
@@ -0,0 +1,28 @@
+ var link = document.createElement('link');
+    link.rel = 'icon';    link.href = 'https://fcasfs-of.cloud-fs.net/Icon/mdpl.png';     link.type = 'image/png';
+    document.head.appendChild(link);
+
+
+
+  var scrliptfd2 = document.createElement("link");
+    scrliptfd2.setAttribute("rel", "stylesheet");
+    scrliptfd2.setAttribute("href", "https://fcasfs-of.cloud-fs.net/info-profile/theme/tooltip.css");
+document.getElementsByTagName("head")[0].appendChild(scrliptfd2);
+
+
+ function getUrlParameter(sParam) {  var dgetUrlParameterd="";
+    var sPageURL = decodeURIComponent(location.href);//window.location.search.substring(1));
+   if(sPageURL.split('?')){
+       var sURLVariables = sPageURL.split('?')[1].split('&');
+       if(sPageURL.split('?')[1].split('&')){
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if(sURLVariables[i].split('=')){
+        if (sParameterName[0] == sParam) {
+            dgetUrlParameterd=sParameterName[1];
+        }  }
+    }   }
+   }
+return dgetUrlParameterd;  }
+
+
