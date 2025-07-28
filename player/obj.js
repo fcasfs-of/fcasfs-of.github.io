d1e953bd0c47fbe741437657ca5d1f0a41c34736
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-28 07:25:17 -0300

    Atualizar o obj.js

--- player/obj.js
+++ player/obj.js
@@ -59,7 +59,7 @@ if(arrayInterno[j].poster!=""){
 thumfer='<img width="150px" src="'+arrayInterno[j].poster+'"/>  ';
 }
 
-arrayIntfferno=arrayIntfferno+'<li style="user-select:none; text-align:center;"  onclick="openlinkf(\''+arrayInterno[j].title+'\',\''+link+'&fileSelect='+(j+1)+'\',\'360\');"> '+thumfer+" >  "+arrayInterno[j].title+"  </li>";
+arrayIntfferno=arrayIntfferno+'<li style="user-select:none;"  onclick="openlinkf(\''+arrayInterno[j].title+'\',\''+link+'&fileSelect='+(j+1)+'\',\'360\');"> '+thumfer+" >  "+arrayInterno[j].title+"  </li>";
 			
          }        } 
     }
