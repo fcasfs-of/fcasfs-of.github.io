07f2835de5f826cced6442b5575597fcc102d7cc
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-08-13 08:17:44 -0300

    Create core_b.js

---
+++ core_b.js
@@ -0,0 +1,27 @@
+ var scrlipftfd2 = document.createElement("link");
+    scrlipftfd2.setAttribute("rel", "stylesheet");
+    scrlipftfd2.setAttribute("href", "core_b.css");
+document.getElementsByTagName("head")[0].appendChild(scrlipftfd2);
+
+
+  const button = document.getElementById('toggle-theme');
+  const body = document.body;
+
+  button.addEventListener('click', () => {
+   // body.classList.toggle('dark');
+    //if (body.classList.contains('dark')) {
+    //  button.textContent = 'Modo Claro';
+// localStorage.setItem('tema', 'escuro');
+    //} else {
+     // button.textContent = 'Modo Escuro';        localStorage.setItem('tema', 'claro');  
+    //}
+  });
+  
+  
+  if(localStorage.getItem('tema') === 'escuro') {
+  // body.classList.add('dark');   button.textContent = 'Modo Claro';
+    } else {
+   //body.classList.remove('dark');      button.textContent = 'Modo Escuro';  
+    }
+
+
