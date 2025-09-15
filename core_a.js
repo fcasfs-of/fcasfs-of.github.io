14d61487a5a4b91ea3a134eba10eace63371540c
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-09-15 08:57:57 -0300

    Update core_a.js

--- core_a.js
+++ core_a.js
@@ -18,26 +18,42 @@ document.getElementsByTagName("head")[0].appendChild(scrlipftfd2);
 
     let temaEscuro = false;
 
-    function alternarTema() {
+    function alternarTema(igf) {
+ var text_q1the='Tema Claro';
+ var text_q2the='Tema Escuro';
+ if(igf && igf=="en"){
+  text_q1the='Theme Light';
+  text_q2the='Theme Dark';
+ }
+     const btngg = document.getElementById('btnthe');
 temaEscuro = !temaEscuro;
       if (temaEscuro) {
         document.body.classList.add('dark');
-         localStorage.setItem('tema', 'escuro');
+        localStorage.setItem('tema', 'escuro');
+        btngg.textContent = text_q1the;
       } else {
         document.body.classList.remove('dark');
-        localStorage.setItem('tema', 'claro');
+        localStorage.setItem('tema', 'claro');    
+        btngg.textContent = text_q2the;
       }
     }
 
 
 
 
-function ir_runtt() {
-    
+function ir_runtt(igf) {
+ var text_q1the='Tema Claro';
+ var text_q2the='Tema Escuro';
+ if(igf && igf=="en"){
+  text_q1the='Theme Light';
+  text_q2the='Theme Dark';
+ }
+
+  const bthngg = document.getElementById('btnthe');
   if(localStorage.getItem('tema') === 'escuro') {
-temaEscuro = true;   document.body.classList.add('dark');
+temaEscuro = true;   document.body.classList.add('dark');   bthngg.textContent = text_q1the;
     } else {
-  temaEscuro = false;  document.body.classList.remove('dark');
+  temaEscuro = false;  document.body.classList.remove('dark');   bthngg.textContent = text_q2the;
     }
    
  }
