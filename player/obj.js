99cc31bc5f7046a61b6055673890183f2b50d58c
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-11-24 11:07:12 -0300

    Update obj.js

--- player/obj.js
+++ player/obj.js
@@ -171,8 +171,6 @@ return arrayIntfferno;  }
   var getfval_tyget=getUrlParameter("fileID");
 	
 if (getfval_tyget!="") {
-
-	if (getfval_tyget=="undefined" || getfval_tyget==undefined) {   loaded_filesa();  }   
 	
   var getfvaddl_tyget=getUrlParameter("cover");
   var getfvaddl_listyget=getUrlParameter("list");
@@ -208,6 +206,6 @@ document.getElementsByTagName("body")[0].appendChild(scriptfd);
 document.getElementsByTagName("body")[0].appendChild(scrfiptfd);
   document.getElementsByTagName("body")[0].onload=function(){  onstart_file(run_file);   remover_id_playerfile();     };
 
-}   
+}   else if (getfval_tyget=="undefined") {   loaded_filesa();  }   
 
 
