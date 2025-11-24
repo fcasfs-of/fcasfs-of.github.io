f6860b55d11c27d00f9df11ddcedc94b01e32736
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-11-24 11:47:45 -0300

    Update obj.js

--- player/obj.js
+++ player/obj.js
@@ -40,7 +40,7 @@ playlist:arquivos_listr, listtotl:arquivos_lisgtr
     listpreview_fleer.innerHTML="";     var styelcopsdsd="";
 var contednfgile= `  ` ;
      if(arquivo.playlist && arquivo.listtotl == 1){    location.href= arquivo.link;   }
-	 else if(arquivo.playlist && arquivo.listtotl == 0){    location.href= arquivo.link;   
+	 else if(arquivo.listtotl == 0){    location.href= arquivo.link;   
      else if(arquivo.playlist && arquivo.listtotl > 1){ 
     styelcopsdsd=' style=" overflow: auto;  "';
     listtitlr.innerHTML=''+arquivo.titulo+''; 
