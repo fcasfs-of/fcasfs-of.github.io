c8d6e3763f78fc1efe1d57bf203c4627e02376ea
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-02-10 11:45:29 -0300

    Create core_ds.js

---
+++ core_ds.js
@@ -0,0 +1,11 @@
+  const fs_subdomains = [
+     {
+      name: "fcasfs-of.cloud-fs.net",
+      links: [
+        { title: "TECH Player", url: "https://player.fcasfs-of.cloud-fs.net/" },
+        { title: "LIGHTBOX", url: "https://lightbox.fcasfs-of.cloud-fs.net/" }
+        
+      ]
+    }
+    
+];
