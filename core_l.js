f09217ce25a6f64f663a346363a0b7aff8bed03f
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-02-10 11:54:29 -0300

    Update core_l.js

--- core_l.js
+++ core_l.js
@@ -1,35 +1,43 @@
   const container = document.getElementById('sitemap-container');
 
 function createSubdomainCard(subdomain) {
+  if(subdomain){
     const card = document.createElement('div');
     card.className = 'subdomain';
 
     const title = document.createElement('h2');
-    title.textContent = subdomain.name;
+    if(subdomain.name){   title.textContent = subdomain.name;    }
     card.appendChild(title);
 
     const ul = document.createElement('ul');
     ul.className = 'links';
 
+    if(subdomain.links){
     subdomain.links.forEach(link => {
+      if(link){
       const li = document.createElement('li');
       const a = document.createElement('a');
-      a.href = link.url;
-      a.textContent = link.title;
+       if(link.url){   a.href = link.url;   }
+       if(link.title){    a.textContent = link.title;    }
       a.target = '_blank'; 
       li.appendChild(a);
       ul.appendChild(li);
+      }
     });
+    }
 
     card.appendChild(ul);
     return card;
+  }
   }
 
 
 if(fs_subdomains){
   fs_subdomains.forEach(sub => {
+      if(sub){
     const card = createSubdomainCard(sub);
     container.appendChild(card);
+      }
   });
   }
 
