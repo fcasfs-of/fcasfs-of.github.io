ac228440ebfd19581791716cf109d7f0e506b44e
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-02-11 10:09:51 -0300

    Update blog_post.js

--- blog_post.js
+++ blog_post.js
@@ -2,12 +2,12 @@ const blogPosts = [
             {
                 category: { pt: "Atualizacao_Projeto", en: "Project_Update" },
                 title: { 
-                    pt: "Nova versão do Player: 3.0.0.1", 
-                    en: "New Player Version: 3.0.0.1" 
+                    pt: "Nova versão do Player: '3.0.0.1'", 
+                    en: "New Player Version: '3.0.0.1'" 
                 },
                 excerpt: { 
-                    pt: "A nova versão inclui:  <br/> <li>Design e desempenho aprimorado.</li><li>Correções de bugs e erros.</li><li>Ícones atualizados.</li> ", 
-                    en: "The new version includes: <br/> <li>Improved design and performance.</li><li>Bug and error fixes.</li><li>Updated icons.</li> " 
+                    pt: "A nova versão inclui:  <br/><br/>  Design e desempenho aprimorado.<br/> Correções de bugs e erros.<br/> Ícones atualizados. ", 
+                    en: "The new version includes: <br/><br/>  Improved design and performance.<br/> Bug and error fixes.<br/>Updated icons. " 
                 },
                 date: "10/02/2026",
                 author: "FCASFS-OF",
