34751ab0c4911623052823a836fd687d81558f12
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-02-11 09:34:23 -0300

    Create blog_post.js

---
+++ blog_post.js
@@ -0,0 +1,17 @@
+const blogPosts = [
+            {
+                category: { pt: "Atualizacao_Projeto", en: "Project_Update" },
+                title: { 
+                    pt: "Nova versão do Player: 3.0.0.1", 
+                    en: "New Player Version: 3.0.0.1" 
+                },
+                excerpt: { 
+                    pt: "A nova versão inclui: design e desempenho aprimorados, correções de bugs e erros e ícones atualizados.", 
+                    en: "The new version includes: Improved design and performance, bug and error fixes, and updated icons." 
+                },
+                date: "11/02/2026",
+                author: "FCASFS-OF",
+                image: "https://player.fcasfs-of.cloud-fs.net/player_banner.jpg"
+            }
+
+];
