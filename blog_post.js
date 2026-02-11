a78620adcb81b178440f150e4786bd92725e576d
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-02-11 10:07:13 -0300

    Update excerpt content for new player version

--- blog_post.js
+++ blog_post.js
@@ -6,11 +6,13 @@ const blogPosts = [
                     en: "New Player Version: 3.0.0.1" 
                 },
                 excerpt: { 
-                    pt: "A nova versão inclui:  <br/> <ul> <li>Design e desempenho aprimorado.</li><li>Correções de bugs e erros.</li><li>Ícones atualizados.</li> </ul>", 
-                    en: "The new version includes: <br/> <ul> <li>Improved design and performance.</li><li>Bug and error fixes.</li><li>Updated icons.</li> </ul>" 
+                    pt: "A nova versão inclui:  <br/> <li>Design e desempenho aprimorado.</li><li>Correções de bugs e erros.</li><li>Ícones atualizados.</li> ", 
+                    en: "The new version includes: <br/> <li>Improved design and performance.</li><li>Bug and error fixes.</li><li>Updated icons.</li> " 
                 },
                 date: "10/02/2026",
                 author: "FCASFS-OF",
+                more: true,
+                link: { pt: "https://player.fcasfs-of.cloud-fs.net/", en: "https://player.fcasfs-of.cloud-fs.net/en" },
                 image: "https://player.fcasfs-of.cloud-fs.net/player_banner.jpg"
             }
 
