9c4d0d061f41b5d457f982c01cf70c691d94cfe3
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-02-12 09:58:15 -0300

    Update core_b.js

--- core_b.js
+++ core_b.js
@@ -1,7 +1,7 @@
 
 const newDilofv = document.createElement("div");
 newDilofv.id = "loaderfs";    newDilofv.className = "loaderffs";
-document.body.getElementsByTagName('main').appendChild(newDilofv);
+document.getElementsByTagName('main')[0].appendChild(newDilofv);
 
 function showPage() {
   document.getElementById("loaderfs").style.display = "none";
