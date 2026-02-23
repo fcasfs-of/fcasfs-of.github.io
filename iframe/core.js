55e85b411c1c396c774f7478474757ddb9d083d7
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-02-23 10:02:01 -0300

    Create core.js

---
+++ iframe/core.js
@@ -0,0 +1,19 @@
+
+function loaded_iframes(){
+  if (cokk_plu_esdnf && langs_cframe){
+const imbjd = iframemanager();
+    imbjd.run({
+        currLang: cokk_plu_esdnf,
+        services: {
+            cloudfs_net: {
+                embedUrl: 'https://{data-id}.cloud-fs.net',
+                thumbnailUrl: 'https://{data-id}.cloud-fs.net/Icon/info.png',
+                iframe: {
+                    allow : 'accelerometer; encrypted-media; gyroscope; picture-in-picture; fullscreen;'
+                },
+                languages: langs_cframe
+            }
+        }
+    });
+  }     }
+
