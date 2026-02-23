479e00dda3d4cd9fdf41971e985dde4798f81286
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-02-23 08:41:14 -0300

    Create langs.js

---
+++ cookies/langs.js
@@ -0,0 +1,89 @@
+var langs_cokkiesd = {
+            pt: {
+                consentModal: {
+                    title: "Utilização de Cookies",
+                    description: "Este site utiliza cookies para melhorar a experiência de navegação e disponibilizar funcionalidades adicionais.",
+                    acceptAllBtn: "Aceitar tudo",
+                    acceptNecessaryBtn: "Rejeitar tudo",
+                    showPreferencesBtn: "Gerenciar preferências",
+                    footer: ""
+                },
+                preferencesModal: {
+                    title: "Central de Preferências de Consentimento",
+                    acceptAllBtn: "Aceitar tudo",
+                    acceptNecessaryBtn: "Rejeitar tudo",
+                    savePreferencesBtn: "Salvar preferências",
+                    closeIconLabel: "Close modal",
+                    serviceCounterLabel: "Service|Services",
+                    sections: [
+                        {
+                            title: "Utilização de cookies",
+                            description: "Nossa plataforma utiliza cookies para aprimorar a sua experiência de navegação, proporcionando um funcionamento mais eficiente e personalizado. Os cookies são pequenos arquivos de texto armazenados no seu dispositivo que nos ajudam a entender suas preferências, melhorar nossos serviços e oferecer conteúdos relevantes. Ao continuar usando nosso site, você concorda com o uso de cookies de acordo com nossa política de privacidade. Você pode gerenciar suas preferências de cookies a qualquer momento nas configurações do seu navegador."
+                        },
+                        {
+                            title: "Cookies estritamente necessários <span class=\"pm__badge\">Sempre ativado</span>",
+                            description: "Cookies estritamente necessários são aqueles essenciais para garantir o funcionamento adequado de um site ou aplicativo. Eles permitem que você navegue pelas páginas, acesse áreas seguras e utilize recursos básicos, como o login ou carrinho de compras. Sem esses cookies, algumas funcionalidades essenciais podem não estar disponíveis, prejudicando sua experiência de uso. Esses cookies geralmente não coletam informações pessoais para fins de marketing ou publicidade, sendo utilizados apenas para assegurar o funcionamento correto do site.",
+                            linkedCategory: "necessary"
+                        },
+                        {
+                            title: "Cookies de funcionalidade",
+                            description: "Cookies de funcionalidade são cookies essenciais que garantem o funcionamento adequado do site, permitindo que recursos e funcionalidades específicas operem corretamente. Eles ajudam a personalizar sua experiência, lembrando suas preferências e configurações, como idioma, região ou opções de acessibilidade. Esses cookies também podem ser usados para fornecer serviços solicitados pelo usuário, como vídeos ou chats ao vivo, garantindo uma navegação mais eficiente e agradável.",
+                            linkedCategory: "functionality"
+                        },
+                        {
+                            title: "Cookies analíticos",
+                            description: "Cookies analíticos são pequenos arquivos de texto utilizados por websites para coletar informações sobre como os usuários interagem com o site. Eles ajudam a entender o desempenho do site, identificando padrões de navegação, número de visitas, páginas mais acessadas e outros dados estatísticos. Essas informações permitem aprimorar a experiência do usuário, otimizar o conteúdo e melhorar a funcionalidade do site, garantindo uma navegação mais eficiente e personalizada. Os cookies analíticos geralmente não armazenam informações pessoais identificáveis, sendo utilizados de forma a preservar a privacidade dos usuários.",
+                            linkedCategory: "analytics"
+                        },
+                        {
+                            title: "Mais informações",
+                            description: "Para qualquer dúvida relacionada à minha política de cookies e às suas opções, entre em contato comigo."
+                        }
+                    ]
+                }
+ },
+            en: {
+                consentModal: {
+                    title: "Use of Cookies",
+                    description: "This website uses cookies to improve the browsing experience and provide additional features.",
+                    acceptAllBtn: "Accept all",
+                    acceptNecessaryBtn: "Reject all",
+                    showPreferencesBtn: "Manage preferences",
+                    footer: ""
+                },
+                preferencesModal: {
+                    title: "Consent Preferences Center",
+                    acceptAllBtn: "Accept all",
+                    acceptNecessaryBtn: "Reject all",
+                    savePreferencesBtn: "Save preferences",
+                    closeIconLabel: "Close modal",
+                    serviceCounterLabel: "Service|Services",
+                    sections: [
+                        {
+                            title: "Cookie Usage",
+                            description: "Our platform uses cookies to enhance your browsing experience, providing more efficient and personalized functionality. Cookies are small text files stored on your device that help us understand your preferences, improve our services, and offer relevant content. By continuing to use our website, you agree to the use of cookies in accordance with our privacy policy. You can manage your cookie preferences at any time in your browser settings."
+                        },
+                        {
+                            title: "Strictly Necessary Cookies <span class=\"pm__badge\">Always Enabled</span>",
+                            description: "Strictly necessary cookies are those essential to ensure the proper functioning of a website or application. They allow you to navigate pages, access secure areas, and use basic features such as logging in or shopping carts. Without these cookies, some essential functionalities may not be available, impairing your user experience. These cookies generally do not collect personal information for marketing or advertising purposes, being used only to ensure the correct functioning of the website.",
+                            linkedCategory: "necessary"
+                        },
+                        {
+                            title: "Functionality Cookies",
+                            description: "Functionality cookies are essential cookies that ensure the proper functioning of the website, allowing specific features and functionalities to operate correctly. They help personalize your experience by remembering your preferences and settings, such as language, region, or accessibility options. These cookies can also be used to provide services requested by the user, such as videos or live chats, ensuring a more efficient and enjoyable browsing experience.",
+                            linkedCategory: "functionality"
+                        },
+                        {
+                            title: "Analytics Cookies",
+                            description: "Analytical cookies are small text files used by websites to collect information about how users interact with the site. They help understand website performance by identifying browsing patterns, number of visits, most accessed pages, and other statistical data. This information allows for improving the user experience, optimizing content, and enhancing website functionality, ensuring more efficient and personalized navigation. Analytical cookies generally do not store personally identifiable information and are used in a way that preserves user privacy.",
+                            linkedCategory: "analytics"
+                        },
+                        {
+                            title: "More information",
+                            description: "For any query in relation to my policy on cookies and your choices, please contact me."
+                        }
+                    ]
+                }
+            }
+        };
+
