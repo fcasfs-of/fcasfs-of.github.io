63dd23346fbe5a1111257d681dccd48cc7aaa99c
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-02-23 10:03:46 -0300

    Create langs.js

---
+++ iframe/langs.js
@@ -0,0 +1,13 @@
+var langs_cframe = {
+                   pt: {
+                        notice: 'Este conteúdo é hospedado por terceiros.',
+                        loadBtn: 'Carregar',
+                        loadAllBtn: "Não pergunte novamente"
+                    },
+                    en: {
+                        notice: 'This content is hosted by a third party.',
+                        loadBtn: 'Load',
+                        loadAllBtn: "Don't ask again"
+                    }
+ };
+
