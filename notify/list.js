d425a7297a592e4f114bacebc6c07c505efd173b
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-02-27 09:54:48 -0300

    Create list.js

---
+++ notify/list.js
@@ -0,0 +1,20 @@
+var test_gnot={
+        title: { pt: '', en: '' },
+        message: { pt: '', en: '' },
+        icon: `` 
+};
+
+
+
+  const notificationsData = {
+    alerts: [
+      
+    ],
+    messages: [
+      
+    ],
+    updates: [
+      
+    ]
+  };
+
