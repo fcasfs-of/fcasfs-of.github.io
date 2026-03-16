29f2396e62bb7733ea2c9ab687d41e9fb8bad090
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-03-16 11:12:08 -0300

    Create test.js

---
+++ notify/test.js
@@ -0,0 +1,9 @@
+ //notifier.tip('This is an example of tip');
+ //notifier.info('This is an example of info');
+ //notifier.alert('This is an example of alert');
+ //notifier.warning('This is an example of warning');
+ //notifier.confirm('You can put any valid HTML here');
+ //notifier.success('This is an example of success');
+ 
+//notifier.modal(`&lt;h3 style='margin-top: 0;'&gt;This is a modal window without any predefined template and styles&lt;/h3&gt;&lt;p&gt;Just put your html into the function and add custom styles&lt;/p&gt;&lt;small&gt;Click outside the modal window to close it.&lt;/small&gt;`, 'demo');
+ 
