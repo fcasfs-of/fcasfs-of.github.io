c3ceec62df0f414e037dc8d3b1836fc4b2c0a2dd
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-04-13 11:41:12 -0300

    Update core_a.js

--- core_a.js
+++ core_a.js
@@ -31,7 +31,7 @@ pt:[
 en:[
 { separate:true, icon:'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">              <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>            </svg>',title:"Portfolio",desc:"Learn more about my professional experience and skills.", links:[{type:"a",link:"https://fcasfs-of.cloud-fs.net/info-profile-en/",text:"Visit the Page"} ]},
 { separate:true, icon:'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">                        <path d="M22 9.24l-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z"/>           </svg>',title:"Projects",desc:"Explore my projects developed in different technologies and programming languages.", links:[{type:"a",link:"https://fcasfs-of.cloud-fs.net/projects",text:"Visit the Page"} ]},
-{ disable:true, separate:true, icon:'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">                          <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"></path>                    </svg>',title:"Download Projects",desc:"Download my complete projects.", links:[{type:"a",link:"https://fcasfs-of.cloud-fs.net/downloads/en",text:"Visit the Page"} ]}
+{ disable:true, separate:true, icon:'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">                          <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"></path>                    </svg>',title:"Download Projects",desc:"Download my complete projects.", links:[{type:"a",link:"https://fcasfs-of.cloud-fs.net/downloads-en",text:"Visit the Page"} ]}
 ]
 }
 
