6eab7fe9648091e8d4fdd12377c1f5b792b2cbb8
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-05-04 08:24:17 -0300

    Update core_b.js

--- core_b.js
+++ core_b.js
@@ -422,6 +422,112 @@ function setup_CookieBadr(){     if(CookieConsent && cokk_plu_esdnf && langs_cok
 }
 
 
+function update_Pagination(asdd){
+if(asdd){
+  if(asdd.contItems && asdd.paginationNumbers && asdd.paginatedList && asdd.listItems && asdd.prevButton && asdd.nextButton && asdd.paginationLimit && asdd.paginationNumberp && asdd.paginationLindex && asdd.paginationclass){  
+
+const contdstItems = document.getElementById(asdd.contItems);
+
+const paginationNumberp = asdd.paginationNumberp;
+const paginationLindex = asdd.paginationLindex;
+const paginationclass = asdd.paginationclass;
+
+const paginationNumbers = document.getElementById(asdd.paginationNumbers);
+const paginatedList = document.getElementById(asdd.paginatedList);
+const listItems = paginatedList.querySelectorAll(asdd.listItems);
+const nextButton = document.getElementById(asdd.nextButton);
+const prevButton = document.getElementById(asdd.prevButton);
+
+paginationNumbers.innerHTML="";
+contdstItems.innerHTML="";
+contdstItems.classList.remove("notpop");
+
+const paginationLimit = asdd.paginationLimit;
+const pageCount = Math.ceil(listItems.length / paginationLimit);
+let currentPage = 1;
+
+
+const disableButton = (button) => {
+  button.classList.add("disabled");
+  button.setAttribute("disabled", true);
+};
+
+const enableButton = (button) => {
+  button.classList.remove("disabled");
+  button.removeAttribute("disabled");
+};
+
+const handlePageButtonsStatus = () => {
+  if (currentPage === 1) {    disableButton(prevButton);  } else {    enableButton(prevButton);  }
+  if (pageCount === currentPage || pageCount < 2 ) {    disableButton(nextButton);  }  else {    enableButton(nextButton);  }
+};
+
+const handleActivePageNumber = () => {
+  document.querySelectorAll(paginationNumberp).forEach((button) => {
+    button.classList.remove("active");
+    const pageIndex = Number(button.getAttribute(paginationLindex));   
+    enableButton(button);   
+    if (pageIndex == currentPage) {      button.classList.add("active");   disableButton(button);       }
+  });
+};
+
+const appendPageNumber = (index) => { 
+  const pageNumber = document.createElement("button");
+  pageNumber.className = paginationclass;
+  var fdappendPageNumber=index;
+  if(pageCount>=10){     if(index<=9){   fdappendPageNumber="0"+index;   }    }
+  pageNumber.innerHTML = fdappendPageNumber;
+  pageNumber.setAttribute("page-index", index);
+  pageNumber.setAttribute("aria-label", "Page " + fdappendPageNumber);
+
+  paginationNumbers.appendChild(pageNumber);
+};
+
+const getPaginationNumbers = () => {
+  for (let i = 1; i <= pageCount; i++) {
+    appendPageNumber(i);
+  }
+};
+
+const setCurrentPage = (pageNum) => {
+  currentPage = pageNum;
+
+  handleActivePageNumber();  handlePageButtonsStatus();
+  
+  const prevRange = (pageNum - 1) * paginationLimit;
+  const currRange = pageNum * paginationLimit;
+
+  listItems.forEach((item, index) => {    item.classList.add("hidden");
+    if (index >= prevRange && index < currRange) {      item.classList.remove("hidden");    }
+  });
+};
+
+contdstItems.classList.add("notpop");
+contdstItems.innerHTML="<hr/>"+listItems.length.toLocaleString('en-US').replace(/,/g, ".");
+if(listItems.length<1){
+contdstItems.classList.remove("notpop");
+contdstItems.innerHTML="Lista de \"Histórico\" está Vazia.";
+}
+
+  getPaginationNumbers();
+  setCurrentPage(1);
+
+  prevButton.addEventListener("click", () => {    setCurrentPage(currentPage - 1);  });
+
+  nextButton.addEventListener("click", () => {    setCurrentPage(currentPage + 1);  });
+
+  document.querySelectorAll(paginationNumberp).forEach((button) => {
+    const pageIndex = Number(button.getAttribute(paginationLindex));
+    if (pageIndex) {
+      button.addEventListener("click", () => {        setCurrentPage(pageIndex);      });
+    }
+  });
+}
+}
+
+}
+
+
 
 
 const body = document.body;
