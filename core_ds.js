d7b5da5d55eb939f67fd33954b51f7d17d82a9a4
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-05-15 08:05:41 -0300

    Update core_ds.js

--- core_ds.js
+++ core_ds.js
@@ -2,8 +2,8 @@
      {
       name: "fcasfs-of.cloud-fs.net",
       links: [
-        { title: "TECH Player", url: "https://player.fcasfs-of.cloud-fs.net/" },
-        { title: "LIGHTBOX", url: "https://lightbox.fcasfs-of.cloud-fs.net/" }
+        { title: "TECH Player", url: "/TECH-Free/" },
+        { title: "LIGHTBOX", url: "/TECH-LightBox/" }
       ]
     }
     
