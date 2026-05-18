e71a8d786d0e7145a3e5991725dbcd645edd578e
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-05-18 08:25:57 -0300

    Update list.js

--- docs/list.js
+++ docs/list.js
@@ -1,7 +1,7 @@
 //    { iconeSvg:"", imagemPreview:"", titulo: "", categoria: "", urlVer: "", urlBaixar: "" },
 
 const meusDocumentos = [
-  { imagemPreview:"tplayer.png", titulo: "Suporte - TECH Player, Português", categoria: "App", urlVer: "/TECH-Free/support-br", urlBaixar: "tech-player-pt.pdf" },
+  { imagemPreview:"tplayer.png", titulo: "Suporte - TECH Player, Português", categoria: "App", urlVer: "/TECH-Free/support-br", urlBaixar: "tech-player-br.pdf" },
   { imagemPreview:"tplayer.png", titulo: "Support - TECH Player, English", categoria: "App", urlVer: "/TECH-Free/support-en", urlBaixar: "tech-player-en.pdf" }
 ];
 
