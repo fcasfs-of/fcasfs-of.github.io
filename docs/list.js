b25f952ed4ec75a130c48b3cc5922776a62448c6
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-05-18 08:22:42 -0300

    Update list.js

--- docs/list.js
+++ docs/list.js
@@ -2,6 +2,6 @@
 
 const meusDocumentos = [
   { imagemPreview:"tplayer.png", titulo: "Suporte - TECH Player, Português", categoria: "App", urlVer: "/TECH-Free/support-br", urlBaixar: "tech-player-pt.pdf" },
-  { imagemPreview:"tplayer.png", titulo: "Support - TECH Player, English", categoria: "App", urlVer: "/TECH-Free/support-en", urlBaixar: "" }
+  { imagemPreview:"tplayer.png", titulo: "Support - TECH Player, English", categoria: "App", urlVer: "/TECH-Free/support-en", urlBaixar: "tech-player-en.pdf" }
 ];
 
