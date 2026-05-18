e1554aa90d0a56e04f8511202a2d158e34b80d00
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-05-17 23:31:35 -0300

    Atualizar o list.js

--- docs/list.js
+++ docs/list.js
@@ -1,7 +1,7 @@
 //    { iconeSvg:"", imagemPreview:"", titulo: "", categoria: "", urlVer: "", urlBaixar: "" },
 
 const meusDocumentos = [
-  { iconeSvg:"", imagemPreview:"tplayer.jpg", titulo: "Suporte - TECH Player", categoria: "App", urlVer: "https://player.fcasfs-of.cloud-fs.net/support-br", urlBaixar: "" },
-{ iconeSvg:"", imagemPreview:"tplayer.jpg", titulo: "Support - TECH Player", categoria: "App", urlVer: "https://player.fcasfs-of.cloud-fs.net/support-en", urlBaixar: "" }
+  { iconeSvg:"", imagemPreview:"tplayer.png", titulo: "Suporte - TECH Player", categoria: "App", urlVer: "https://player.fcasfs-of.cloud-fs.net/support-br", urlBaixar: "" },
+{ iconeSvg:"", imagemPreview:"tplayer.png", titulo: "Support - TECH Player", categoria: "App", urlVer: "https://player.fcasfs-of.cloud-fs.net/support-en", urlBaixar: "" }
 ];
 
