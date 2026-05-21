f827d73ae7a551ec77bb23a7994db66532628b05
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-05-21 09:38:31 -0300

    Update core_b.js

--- core_b.js
+++ core_b.js
@@ -488,7 +488,7 @@ function setup_CookieBadr(){     if(CookieConsent && cokk_plu_esdnf && langs_cok
     }
 });   
 	
-cookies_pre = getObj_nInfo({ local: "button", text:'[data-role="show"]' })//getObj_nInfoEx();
+cookies_pre = getObj_nInfo({ local: "button", text:'[data-role="show"]' });//getObj_nInfoEx
 }   }
 
 
