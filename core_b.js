058dfdba46cb1bbea183e082ce73b792ce792882
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-05-29 09:48:29 -0300

    Update core_b.js

--- core_b.js
+++ core_b.js
@@ -114,7 +114,7 @@ function fs_pagination(config) {
     btnAnterior.className = 'pag-btn';
     btnAnterior.innerHTML = svgAnterior;
     btnAnterior.disabled = paginaAtual === 1;
-    btnAnterior.onclick = () => { paginaAtual--;  listaContainer.scrollIntoView({ behavior: "smooth", block: "start" });  atualizar();  };
+    btnAnterior.onclick = () => { paginaAtual--;   if(Catalogo_prj){Catalogo_prj.filtrar("Todos");Catalogo_prj.filtrar("All");}   listaContainer.scrollIntoView({ behavior: "smooth", block: "start" });  atualizar();  };
     nav.appendChild(btnAnterior);
 
     for (let i = 1; i <= totalPaginas; i++) {
@@ -122,7 +122,7 @@ function fs_pagination(config) {
       btnNum.className = `pag-btn ${i === paginaAtual ? 'ativo' : ''}`;
       btnNum.innerText = i;
 	  btnNum.id = `sh_page_${i}`;
-      btnNum.onclick = () => { paginaAtual = i;  listaContainer.scrollIntoView({ behavior: "smooth", block: "start" });  atualizar();  };
+      btnNum.onclick = () => { paginaAtual = i;   if(Catalogo_prj){Catalogo_prj.filtrar("Todos");Catalogo_prj.filtrar("All");}   listaContainer.scrollIntoView({ behavior: "smooth", block: "start" });  atualizar();  };
       nav.appendChild(btnNum);
     }
 
@@ -130,7 +130,7 @@ function fs_pagination(config) {
     btnProximo.className = 'pag-btn';
     btnProximo.innerHTML = svgProximo;
     btnProximo.disabled = paginaAtual === totalPaginas;
-    btnProximo.onclick = () => { paginaAtual++;  listaContainer.scrollIntoView({ behavior: "smooth", block: "start" });  atualizar();  };
+    btnProximo.onclick = () => { paginaAtual++;   if(Catalogo_prj){Catalogo_prj.filtrar("Todos");Catalogo_prj.filtrar("All");}   listaContainer.scrollIntoView({ behavior: "smooth", block: "start" });  atualizar();  };
     nav.appendChild(btnProximo);
 
   }
