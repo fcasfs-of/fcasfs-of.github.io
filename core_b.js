e1d5e4eb83c1c486a99a8d4fb526d3e8a198c8e2
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-06-11 16:42:34 -0300

    Update core_b.js

--- core_b.js
+++ core_b.js
@@ -423,6 +423,8 @@ var cookies_pre = { title: "Cookies", desc: "", icon:'', open: null };
 var storage_ps;
 
 function storage_ps_crtew(cokk_plu_esdnf) {
+	if (typeof StorageInspector_ON === 'function'){
+StorageInspector_ON();
 function storage_ps_show(laj) {
 	if (typeof analisadorAPI.open === 'function'){   analisadorAPI.open(laj, { fullscreen: false, backup: true, close: true }, true, {
     onOpen: (dominio) => {    },
@@ -439,7 +441,7 @@ function storage_psen_show() {  storage_ps_show("en");  }
 storage_ps = { title: "Inspetor de Armazenamento", desc: "", icon:'<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align:middle"><path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z M2 12h20 M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>', open:storage_pspt_show };
 if(cokk_plu_esdnf && cokk_plu_esdnf=="en"){
 storage_ps = { title: "Storage Inspector", desc: "", icon:'<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align:middle"><path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z M2 12h20 M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>', open:storage_psen_show };
-}
+}    }
 }
 
 
