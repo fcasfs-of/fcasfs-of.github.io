b6806458280a0d17d8dc862cc83a383e05f5c72a
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-06-12 10:56:23 -0300

    Update core_ds.js

--- core_ds.js
+++ core_ds.js
@@ -12,15 +12,27 @@
 
 const fs_subdomains_pp = [
     {
-     name: "",
+     name: "01",
      links: [
        { title: "ZIP_C", url: "/zip_c/" },
-       { title: "DCBS", url: "/DCBS/" },
        { title: "MP4_QA", url: "/mp4_qa/" },
-       { title: "MP3_MP", url: "/mp3_mp/" },
+       { title: "MP3_MP", url: "/mp3_mp/" }
+      ]
+    }, {
+     name: "02",
+     links: [
+       { title: "DCBS", url: "/DCBS/" },
+       { title: "PaletteApp", url: "/PaletteApp/" }
+      ]
+    }, {
+     name: "03",
+     links: [
        { title: "GradientS", url: "/GradientS/" },
-       { title: "WebCompile", url: "/WebCompile/" },
-       { title: "PaletteApp", url: "/PaletteApp/" },
+       { title: "WebCompile", url: "/WebCompile/" }
+      ]
+    }, {
+     name: "04",
+     links: [
        { title: "Search Files in Folders", url: "/Search-Files-in-Folders/", disable: true },
        { title: "TECH-SideBar", url: "/TECH-SideBar/" }
       ]
