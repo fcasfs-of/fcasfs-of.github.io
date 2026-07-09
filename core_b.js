b9d331571d2f5b972a5a832e8d31b7a8514b7249
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-07-09 11:40:43 -0300

    Update core_b.js

--- core_b.js
+++ core_b.js
@@ -180,13 +180,13 @@ function share_btsD(att, listd, listdld, opti, id){ var add_vjif = (id && docume
 
 function share_btsD_all(fg){   
  share_btsD([["data-via","website"],["data-title",""],["data-media",""],["data-url",""],["data-hashtags","fcasfs-of"]], [ "bluesky","facebook","fediverse","hackernews","linkedin","lobsters","mastodon","messenger","odnoklassniki","pinterest","pocket","reddit","teams","telegram","tumblr","twitter","threads","viber","vkontakte","whatsapp","separate","copy-url","email","print","separate","web-share" ], { messenger:[["data-fb-app-id",""]] }, { icon:"true", text:"false" }, "");    
- if(Shareon){  Shareon.init();   }
-
 if(fg && fg!=""){
 	var share_btsD_all_yd="Gostou dos meus projetos? Compartilhe meu portfólio com a sua rede usando os botões abaixo!";
 	if(fg=="en"){  share_btsD_all_yd="Liked my projects? Share my portfolio with your network using the buttons below!";  }
 	fsummary_Add("shareon_main", share_btsD_all_yd, '<svg fill="none" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21.707,11.293l-8-8A.99991.99991,0,0,0,12,4V7.54492A11.01525,11.01525,0,0,0,2,18.5V20a1,1,0,0,0,1.78418.62061,11.45625,11.45625,0,0,1,7.88672-4.04932c.0498-.00635.1748-.01611.3291-.02588V20a.99991.99991,0,0,0,1.707.707l8-8A.99962.99962,0,0,0,21.707,11.293ZM14,17.58594V15.5a.99974.99974,0,0,0-1-1c-.25488,0-1.2959.04932-1.56152.085A14.00507,14.00507,0,0,0,4.05176,17.5332,9.01266,9.01266,0,0,1,13,9.5a.99974.99974,0,0,0,1-1V6.41406L19.58594,12Z"/></svg>');
 }
+	if(Shareon){  Shareon.init();   }
+
 }
 
 function micAccessTool_run(cokk_plu_esdnf){ 
