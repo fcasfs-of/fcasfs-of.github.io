96fe82620a0b6d04f5587a9560ba858f2659ccd8
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-07-13 08:31:26 -0300

    Update core_b.js

--- core_b.js
+++ core_b.js
@@ -4,7 +4,7 @@ function fsummary_accAdd(val, titler, icons) {
   if (val === true && titler) {
     let iconHtml = '';
     if (icons) {      iconHtml = '<div class="fjs-divider-icon">' + icons + '</div>';    }
-    return '<br/><br/><div class="fjs-divider"><div class="fjs-divider-line"></div><div class="fjs-divider-content">' + iconHtml + '<span class="fjs-divider-text">' + titler + '</span></div><div class="fjs-divider-line"></div></div>';
+    return '<br/><br/><div class="fjs-divider"><div class="fjs-divider-line"></div><div class="fjs-divider-content">' + iconHtml + '<span class="fjs-divider-text" style="text-transform: none;">' + titler + '</span></div><div class="fjs-divider-line"></div></div>';
   }
   return '<br/><br/><div class="fjs-divider"><div class="fjs-divider-line"></div> <div class="fjs-divider-line"></div></div>';
 }
