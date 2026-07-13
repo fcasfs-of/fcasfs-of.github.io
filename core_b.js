d5f94adf4f5799d4db880d8419d82f0235f14536
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-07-13 08:27:56 -0300

    Update core_b.js

--- core_b.js
+++ core_b.js
@@ -14,7 +14,7 @@ function fsummary_Add(seletor, texto, item) {
     const elemento = document.querySelector("."+seletor);
         if (elemento) {   
       const novoSpan = document.createElement('div');
-		novoSpan.style.cssText = 'width: 95%;    overflow: auto;    margin: 0 auto;    text-align: center;";
+		novoSpan.style.cssText = 'width: 95%;    overflow: auto;    margin: 0 auto;    text-align: center;';
         novoSpan.innerHTML = '' + fsummary_accAdd(true, texto, item) + '';
       elemento.parentElement.insertBefore(novoSpan, elemento);
     }
