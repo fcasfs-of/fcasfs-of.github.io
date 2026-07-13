e6e7f02487b9caeca2440e4347578739e3a6d65a
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-07-13 08:16:50 -0300

    Update core_b.js

--- core_b.js
+++ core_b.js
@@ -11,7 +11,7 @@ return fsummary_accAddadd;   }
 
 function fsummary_Add(seletor, texto, item) {
   if (typeof seletor === 'string' && seletor!="") {
-    const elemento = document.querySelector(seletor);
+    const elemento = document.querySelector("."+seletor);
         if (elemento) {   
       const novoSpan = document.createElement('span');
           novoSpan.innerHTML = '' + fsummary_accAdd(true, texto, item) + '';
