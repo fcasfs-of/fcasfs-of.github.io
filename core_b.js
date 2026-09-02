f34474f0d6f052975b1bb59fa13f89db24c63fa2
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-09-02 11:42:01 -0300

    Update core_b.js

--- core_b.js
+++ core_b.js
@@ -281,7 +281,11 @@ if(cokk_plu && cokk_plu==true){
  if (location.href.toUpperCase().indexOf(cokk_plu_enfdd.toUpperCase()) > -1) {    cokk_plu_esdnf="en";    }
 
 
-var ads_pre = function(){}; function start_adsPg(ff){ ads_pre = function(list_ads, liston, th, eb){ if(list_ads && liston && typeof fcasfs_ads === 'function'){ fcasfs_ads({ theme: th || 'light', embed: eb || false, lang: ff || "pt", targets: liston || [], list: list_ads || [] }); } }; }
+var ads_pre = function(){}; 
+function start_adsPg(ff){ ads_pre = function(list_ads, liston, th, eb){ if(list_ads && liston && typeof fcasfs_ads === 'function'){ fcasfs_ads({ theme: th || 'light', embed: eb || false, lang: ff || "pt", targets: liston || [], list: list_ads || [] }); } }; }
+
+var bookmarkmanages_pre={title:"Bookmark",desc:"",icon:"",open:null};
+
 
 
 carregarTudo([
@@ -405,7 +409,8 @@ carregarTudo([
     }, {
         destino: 'body',  tag: 'script',   
         atributos: {    
-		    'onload': "gerenciarAreaFav('"+cokk_plu_esdnf+"');",
+		    'onload': "gerenciarAreaFav_start('"+cokk_plu_esdnf+"');",
+             'defer': "",  'init': "",
             'src': 'https://fcasfs-of.cloud-fs.net/player/fav.js'
         }
     },  {
@@ -431,10 +436,8 @@ carregarTudo([
 }
 
 
-var bookmarkmanages_pre={title:"Bookmark",desc:"",icon:"",open:null};
 function gerenciarAreaFav(idioma){if(typeof initBookmarkManager!=='function'||!document.body)return;var divPai=document.getElementById("areafav");if(!divPai){divPai=document.createElement("div");divPai.id="areafav";var divFilho=document.createElement("div");divFilho.id="areafavlist";var style=document.createElement("style");style.textContent=".bkm-main-wrapper, .bkm-main-wrapper div, .bkm-main-wrapper div button { pointer-events: none; display: none; }";divPai.appendChild(style);divPai.appendChild(divFilho);document.body.appendChild(divPai);var initBookmarkManager_mo=initBookmarkManager({containerId:"areafavlist",targetMode:"modal",lang:idioma==="pt"?"pt":"en",startMaximized:true});if(initBookmarkManager_mo){window.bookmarkmanages_pre={title:idioma==="pt"?"Gerenciador de Favoritos":"Bookmark Manager",icon:'<svg viewBox="0 0 24 24" width="22" height="22" fill="none" stroke="currentColor" stroke-width="2"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>',open:typeof initBookmarkManager_mo.abrir==="function"?initBookmarkManager_mo.abrir:null};}}}
 function gerenciarAreaFav_start(idioma){gerenciarAreaFav(idioma);  document.body.onload=function(){ gerenciarAreaFav(idioma);  };  }
-
 
 
 function create_tooltipr(op){ var create_infoxrs=""; if (op){ if (op.text && op.text!=""){ var create_infoxrcolor=""; var dcreate_infoxrcolor="top"; if (op.visible && op.visible==true){ create_infoxrcolor=' data-ver="true"'; } if (op.pos && op.pos!=""){ dcreate_infoxrcolor=""+op.pos; } create_infoxrs=' data-tooltip="'+op.text+'" data-flow="'+dcreate_infoxrcolor+'"'+create_infoxrcolor; } } return create_infoxrs; }
