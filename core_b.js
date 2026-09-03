1530176a73cc0a57b53b34c07931638d472527e1
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-09-03 11:15:32 -0300

    Update core_b.js

--- core_b.js
+++ core_b.js
@@ -200,7 +200,12 @@ function showPage() {
 function laoded_call_ff(){   loaded_maut();   initAvisos(initAvisos_onload);    }
 
 carregarTudo([
-    {
+     {
+        destino: 'body',  tag: 'script',   
+        atributos: {    
+            'src': 'https://lightbox.fcasfs-of.cloud-fs.net/app/api.js'
+        }
+    }, {
         destino: 'body',  tag: 'script',   
         atributos: {    
             'onload': 'laoded_call_ff(); ',
